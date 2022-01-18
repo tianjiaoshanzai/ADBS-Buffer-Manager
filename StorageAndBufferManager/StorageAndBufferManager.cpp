38c4d8b01cbebd8003c9a55fbae4f001f6f56d33
yxs <yxs@LAPTOP-DIBAI4BS>
2022-01-18 09:37:00 +0800

    添加项目文件。

---
+++ StorageAndBufferManager/StorageAndBufferManager.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+//-----------------初始参数开始--------------------------------------------------//
+#define FRAMESIZE 4096 //frame大小
+#define DEFBUFSIZE 1024 //buffer默认大小，用户可后续自定义
+//-----------------初始参数结束-------------------------------------------------//
+
+
+
+struct bFrame
+{
+    char field[FRAMESIZE];
+};
+
+bFrame buf[DEFBUFSIZE];
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
